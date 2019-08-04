91a1dd7aa5ec78d0e381e8547f8660c8c2601aea
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 14:48:45 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card
-  deal_card
+  cardOne = deal_card
+  cardTwo = deal_card
   card_total = cardOne + cardTwo 
   display_card_total(card_total)
   return card_total
