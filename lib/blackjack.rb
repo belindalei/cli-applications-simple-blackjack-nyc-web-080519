a177c1226ea0df55d4bda9507244a85877e9d786
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 14:46:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,10 +35,12 @@ def hit?(card_total)
   get_user_input 
   if get_user_input === 'h'
     card_total += deal_card
+  end 
+    card_total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts 
 end
 
 #####################################################
